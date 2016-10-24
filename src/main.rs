1c3276ae23f3a7c05d18d202eca1dd582baeef80
Dominik Menzi <dmenzi@tangresh.ch>
2016-10-24 20:38:02 +0200

    Complete game logic

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
+use std::collections::HashSet;
 use std::ops::Index;
 
-#[derive(PartialEq, Eq, Debug)]
+#[derive(PartialEq, Eq, Hash, Debug)]
 struct Position {
     x: i64,
     y: i64
@@ -14,13 +15,13 @@ impl Position {
         }
     }
 
-    fn get_neighbours(&self) -> Vec<Position> {
-        let mut neighbours = vec!();
+    fn get_neighbours(&self) -> HashSet<Position> {
+        let mut neighbours = HashSet::new();
         for x in -1..2 {
             for y in -1..2 {
                 let neighbour = Position::new(self.x + x, self.y + y);
-                if (&neighbour != self) {
-                    neighbours.push(neighbour);
+                if &neighbour != self {
+                    neighbours.insert(neighbour);
                 }
             }
         }
@@ -47,14 +48,36 @@ impl GameBoard {
     }
 
     fn advance_time(&mut self) {
-        let mut new_generation: Vec<Position> = vec!();
+        let mut new_generation: HashSet<Position> = HashSet::new();
         {
             let current_generation = self.get_current_generation();
-            for pos in current_generation.live_cells() {
-                let neighbours = pos.get_neighbours();
+            let interesting_positions = self.get_interesting_positions();
+            for interesting_position in interesting_positions {
+                let mut count = 0;
+                for neighbour in interesting_position.get_neighbours() {
+                    if (current_generation.live_cells().contains(&neighbour)) {
+                        count = count + 1;
+                    }
+                }
+                if (count == 3) {
+                    new_generation.insert(interesting_position);
+                }
+                else if (count == 2 && current_generation.live_cells().contains(&interesting_position)) {
+                    new_generation.insert(interesting_position);
+                }
             }
         }
-        self.generations.push(Generation::new(vec!()))
+        self.generations.push(Generation::new(new_generation))
+    }
+
+    fn get_interesting_positions(&self) -> HashSet<Position> {
+        let mut interesting_positions = HashSet::new();
+        let current_generation = self.get_current_generation();
+        for pos in current_generation.live_cells() {
+            let neighbours = pos.get_neighbours();
+            interesting_positions.extend(neighbours);
+        }
+        interesting_positions
     }
 }
 impl WithLiveCells for GameBoard {
@@ -64,10 +87,10 @@ impl WithLiveCells for GameBoard {
 }
 
 struct Generation {
-    live_positions: Vec<Position>
+    live_positions: HashSet<Position>
 }
 impl Generation {
-    fn new(live_positions: Vec<Position>) -> Generation {
+    fn new(live_positions: HashSet<Position>) -> Generation {
         Generation {
             live_positions: live_positions
         }
@@ -75,11 +98,11 @@ impl Generation {
 
     fn build() -> GenerationBuilder {
         GenerationBuilder{
-            live_positions: vec!()
+            live_positions: HashSet::new()
         }
     }
 
-    fn live_cells(&self) -> &Vec<Position> {
+    fn live_cells(&self) -> &HashSet<Position> {
         &self.live_positions
     }
 }
@@ -90,12 +113,12 @@ impl WithLiveCells for Generation {
 }
 
 struct GenerationBuilder {
-    live_positions: Vec<Position>
+    live_positions: HashSet<Position>
 }
 impl GenerationBuilder {
     fn add(mut self, x: i64, y: i64) -> GenerationBuilder {
         let pos = Position::new(x, y);
-        self.live_positions.push(pos);
+        self.live_positions.insert(pos);
         self
     }
 
@@ -110,6 +133,7 @@ impl GenerationBuilder {
 #[cfg(test)]
 mod tests {
     use expectest::prelude::*;
+    use std::collections::HashSet;
     use super::Position;
     use super::WithLiveCells;
     use super::GameBoard;
@@ -134,7 +158,7 @@ mod tests {
     fn position_should_have_8_neighbours() {
         let pos = Position::new(0, 0);
 
-        let neighbours: Vec<Position> = pos.get_neighbours();
+        let neighbours: HashSet<Position> = pos.get_neighbours();
 
         expect!(neighbours.len()).to(be_equal_to(8));
     }
@@ -190,5 +214,21 @@ mod tests {
         expect!(current_generation.is_alive(&Position::new(0, 1))).to(be_true());
         expect!(current_generation.is_alive(&Position::new(1, 0))).to(be_true());
         expect!(current_generation.is_alive(&Position::new(1, 1))).to(be_true());
+    }
+
+    #[test]
+    fn cell_with_3_neighbours_should_spawn() {
+        let mut game_board = GameBoard::initialize_with(
+            Generation::build()
+                .add(0, 1)
+                .add(1, 0)
+                .add(1, 1)
+                .build());
+
+        game_board.advance_time();
+
+        let current_generation = game_board.get_current_generation();
+
+        expect!(current_generation.is_alive(&Position::new(0, 0))).to(be_true());
     }
 }
